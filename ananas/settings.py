a59d3ba34197b5aafd0f13e295de4d938badd3e9
Your Name <you@example.com>
2023-05-05 19:06:55 +0600

    change db to postgres, add to req psycopg2

--- ananas/settings.py
+++ ananas/settings.py
@@ -106,13 +106,24 @@ AUTH_USER_MODEL = 'user.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ananas_db',
+        'USER': 'ananas_user',
+        'PASSWORD': 'qwe123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
